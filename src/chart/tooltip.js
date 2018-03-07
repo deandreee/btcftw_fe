edcb791b28bccc8e295f7513ef4e788a41c58557
gb <guns.blazin.7@gmail.com>
2018-03-07 20:41:50 +0200

    tooltip position to allow enter and click open reddit

--- src/chart/tooltip.js
+++ src/chart/tooltip.js
@@ -1,6 +1,19 @@
 import styles from 'app/styles';
 import md from 'utils/md'
 
+let positionDesktop = (pt) => {
+  // console.log(pt);
+  let [ x, y ] = pt;
+  // return [ x - 10, y - 1 ];
+  return [ x, y ];
+  // return [ x + 20, y + 20 ];
+  // return null;
+}
+
+let positionMobile = (pt) => {
+  return [10, 10];
+}
+
 export default {
   show: true,
   trigger: 'axis', // [ default: 'item' ]
@@ -9,8 +22,9 @@ export default {
   borderWidth: 3,
   textStyle: { fontFamily: styles.fontFamily },
   backgroundColor: 'rgba(55,55,55,0.9)', // default opacity 0.7, let's make a bit more dark
-  position: md.phone() ? function (pt) { return [10, 10]; } : null,
-  // enterable: true,
+  position: md.phone() ? positionMobile : positionDesktop,
+
+  enterable: true, // need this so mouseover stops working while inside tooltip
   // showDelay: 500,
-  // hideDelay: 5000 // doesn't really change anything
+  hideDelay: 1000 // doesn't really change anything
 }
